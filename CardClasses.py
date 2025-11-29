143980b5c6aa1e4a111f8b77436f70da200e9f04
ArthurSarturi <seyycookie@gmail.com>
2025-11-29 18:33:48 -0300

    DOCS: criação de uma documentação no codigo

--- CardClasses.py
+++ CardClasses.py
@@ -3,54 +3,60 @@ import random
 
 #  CLASSES BASES  #
 
+#classe que sera herdada pelos objetos cartas
+
 class Card:
     def __init__(self, name, effect, price):
-        self.name = name
-        self.effect = effect
-        self.price = price
+        self.name = name # nome
+        self.effect = effect # efeito
+        self.price = price # preço
 
     def __repr__(self):
         return f"{self.name} | {self.effect} | {self.price} coins"
+    # retorna uma string mutcho loka, (eu acho).
 
+# classe de gerenciamento dos baralhos
 
 class Deck:
-    def __init__(self, cards=None):
-        self.cards = cards if cards else []
+    def __init__(self, cards=None): # inicia :)  / permite iniciar o deck ja com cartas ou vazio.
+        self.cards = cards if cards else [] # se ele mandou o valor de cards, entao ele vai usar o valor de cards, caso nao. recebe lista vaziaaaaaa!
 
-    def add_card(self, card):
+    def add_card(self, card): ## adiciona cartas ao baralho
         self.cards.append(card)
 
-    def remove_card(self, index):
+    def remove_card(self, index): #retira a cartas do baraio
         return self.cards.pop(index)
 
-    def is_full(self):
+    def is_full(self): # vereifica se ta cheio ou nao, retornando uma bool (true or false miojo :)
         return len(self.cards) >= 3
 
-    def __repr__(self):
+    def __repr__(self): # retorna string mutch loka que é imprimida
         return f"{self.cards}"
 
 
-class Player:
+class Player: # classe do jogador
     def __init__(self, name, starting_deck):
-        self.name = name
-        self.deck = Deck(starting_deck)
-        self.coins = 15
-        self.health = 4
+        self.name = name # seu nome
+        self.deck = Deck(starting_deck) # o deck que ele recebe é o inicial
+        self.coins = 15 # denero
+        self.health = 4 # vida
 
-    def show_deck(self):
+    def show_deck(self): # isso faz com que ele mostre os itens do deck dele de forma numerada
         print("Seu Deck:")
-        for i, card in enumerate(self.deck.cards, 1):
+        for i, card in enumerate(self.deck.cards, 1): ## for que busca todos os itens dentro de cards.
             print(f"{i} - {card}")
 
-    def buy_card(self, store, index):
-        card = store.cards[index]
-        if self.deck.is_full():
-            print("Inventário cheio!")
-            return False
+    ## escrever a documentação dessa forma talvez de problema.
 
-        if self.coins >= card.price:
-            self.coins -= card.price
-            self.deck.add_card(card)
+    def buy_card(self, store, index): # compra cartas
+        card = store.cards[index] # card, no caso, a carta escolhida na posição definida pelo index
+        if self.deck.is_full(): # verifica se o deck do jogador ta chei
+            print("Inventário cheio!")
+            return False 
+
+        if self.coins >= card.price: # se tiver dinhheiro o suficiente
+            self.coins -= card.price ## remove o custo da carta do denero do jogador
+            self.deck.add_card(card) # adiciona ela ao seu deck
             print(f"Você comprou {card.name}")
             return True
         else:
@@ -58,86 +64,92 @@ class Player:
             return False
 
 
-class Store:
-    def __init__(self, cards):
-        self.cards = cards
+class Store: ## loja!!!
+    def __init__(self, cards): 
+        self.cards = cards ## cartas do jogador
 
-    def show(self):
+    def show(self): ## mostra as cartas disponiveis e um display bacanudo
         print("|   CARTA   |   FUNÇÃO   |   PREÇO   | ")
-        for i, card in enumerate(self.cards, 1):
+        for i, card in enumerate(self.cards, 1): # for que busca cartas disponiveis pra venda na loja
             print(f"{i} - {card}")
 
 #  SISTEMA DE LOCAIS  #
 
-class Location:
+class Location: ## classe das localizações de onde o player pode ir
     def __init__(self, name, enemy_name, enemy_deck, enemy_life, reward):
-        self.name = name
-        self.enemy_name = enemy_name
-        self.enemy_deck = enemy_deck
-        self.enemy_life = enemy_life
-        self.reward = reward
+        self.name = name # nome
+        self.enemy_name = enemy_name # nome do inimigo da area
+        self.enemy_deck = enemy_deck # deck do bixo
+        self.enemy_life = enemy_life # vida do bixo
+        self.reward = reward # recompensa por vencer o bixo
 
-    def __repr__(self):
+    def __repr__(self): # retorna uma string mutcho loka com o nome da area
         return f"{self.name}"
 
 
 #  SISTEMA DE COMBATE DAHORA  #
 
 
-class Combat:
-    def __init__(self, player, location):
-        self.player = player
-        self.enemy_name = location.enemy_name
+class Combat: ## SISTEMA DE COMBATEEE!
+    def __init__(self, player, location): # pede o player e a localização | Area
+        self.player = player # player
+        self.enemy_name = location.enemy_name # pega as informaçoes do inimigo
         self.enemy_life = location.enemy_life
         self.enemy_deck = location.enemy_deck
         self.reward = location.reward
         self.player_life = player.health
 
-    def show_scene(self, pa, ea):
+    def show_scene(self, pa, ea): # mostra a cena e limpa o terminal
+        ## pa = playerAction | ea EnemyAction
         os.system('cls')
-        print(f"| VIDA: {self.player_life} | vs | {self.enemy_life} :VIDA |")
+        print(f"| VIDA: {self.player_life} | vs | {self.enemy_life} :VIDA |") ## cria um display no terminal
         print(f"Sua ação: {pa} | Ação inimiga: {ea}")
 
-    def get_player_action(self):
-        self.player.show_deck()
-        while True:
-            choice = input("Escolha sua carta: ")
-            if choice.isdigit() and 1 <= int(choice) <= len(self.player.deck.cards):
-                return self.player.deck.cards[int(choice)-1].name
+    def get_player_action(self): ## pega a ação do jogador
+        self.player.show_deck() ## mostra o deck do jogador
+        while True: ## looping enquanto a batalha durar
+            choice = input("Escolha sua carta: ") # input de escolha de carta
+            if choice.isdigit() and 1 <= int(choice) <= len(self.player.deck.cards): # verificação de integridade da escolha
+                return self.player.deck.cards[int(choice)-1].name 
             print("Escolha inválida!")
 
-    def get_enemy_action(self):
-        return random.choice(self.enemy_deck).name
+    def get_enemy_action(self): ## ação do inimigo
+        return random.choice(self.enemy_deck).name #aleatoriza a carta do inimigo e retorna o nome da messsssssssma carta!
 
-    def resolve_round(self, action1, action2):
-        win_map = {
+    def resolve_round(self, action1, action2): #calculo de batalha
+        win_map = { # basicamente é o mesmo calculo que acontece no pedrao papeu tesoura, exemplo:
+            # papel vence de? Pedra!
             "Espada": ["Escudo"],
             "Escudo": ["Arco"],
             "Arco": ["Espada", "Espada Grande"],
             "Espada Grande": ["Espada", "Escudo"]
         }
 
+        # ve se eles fizeram a mesma coisa
         if action1 == action2:
             print("Empate!")
             return
 
+        # ve se a carta do inimigo (action2) esta na lista de derrotadas pela carta do jogador (action1).
         if action2 in win_map.get(action1, []):
-            self.enemy_life -= 1
+            self.enemy_life -= 1 # voce da um de dano no inimigo
             print("Você venceu a rodada!")
-        else:
-            self.player_life -= 1
+        else: ## caso contraaario :D
+            self.player_life -= 1 # inimigo da 1 de dano no jogador
             print("Você perdeu a rodada!")
 
-    def start(self):
-        print(f"Você entrou em combate contra {self.enemy_name}!")
+    def start(self): # inicia a batalha
+        print(f"Você entrou em combate contra {self.enemy_name}!") # texto inicial
 
-        while self.player_life > 0 and self.enemy_life > 0:
-            pa = self.get_player_action()
-            ea = self.get_enemy_action()
+        while self.player_life > 0 and self.enemy_life > 0: # repete enquanto os dois estiverem vivos
+            pa = self.get_player_action() # pega ação do jogador
+            ea = self.get_enemy_action() # pega ação do inimigo
 
+            # mostra a cena e calcula seu resultado
             self.show_scene(pa, ea)
             self.resolve_round(pa, ea)
 
+        ## conclusao da batalha com a verificação de resultado
         if self.enemy_life <= 0:
             print("Você derrotou o inimigo!")
             print(f"Recompensa: {self.reward} coins")
--- CardGame.py
+++ CardGame.py
@@ -1,20 +1,27 @@
+## IMPORT DAS CLASSES CRIADAS NO CADRCLASSES.PY ##
+
 from CardClasses import Store
 from CardClasses import Card
 from CardClasses import Location
 from CardClasses import Player
 from CardClasses import Combat
 
+## SISTEMA OPERACIONAL PARA MEXER NO TERMINAL :)
 import os
 
-class Game:
+## CLASSE GAME, CONTROLA TODO O FLUXO DE GAMEPLAY LEGAL ##
+
+class Game: ## cria classe controladora do jogo :)
+    
     def __init__(self):
-        self.cards = self.create_cards()
+        self.cards = self.create_cards() 
         self.starter_decks = self.create_starter_decks()
         self.store = Store(self.create_store_cards())
         self.locations = []
         self.create_locations()
         self.player = None
 
+    ## retorna as cartas disponiveis no jogo, com suas caracteristicas respectivas aos atributos da classe.
     def create_cards(self):
         return {
             "sword": Card("Espada", "Vence Escudos e Adagas. Perde para Arco e Espada Grande", 5),
@@ -23,38 +30,41 @@ class Game:
             "greatsword": Card("Espada Grande", "Vence Espadas e Escudos. Perde para Arco", 30)
         }
 
-    def create_starter_decks(self):
-        c = self.cards
+    ## cria os baralhos iniciais, sendo eles cada um uma lista.
+    def create_starter_decks(self): 
+        c = self.cards 
         return [
             [c["sword"], c["shield"]],
             [c["shield"], c["sword"]]
         ]
 
+    ## Cartas disponiveis para a compra. retorna uma lista dos itens
     def create_store_cards(self):
         c = self.cards
         return [c["bow"], c["sword"], c["shield"], c["greatsword"]]
 
+    ## cria as localizaçoes, passando os atributos da classe LOCATION.
     def create_locations(self):
         c = self.cards
-        # locais originais
-        self.locations.append(Location(
-            "Masmorra Sombria", 
-            "Carrasco Imaculado",
-            [c["sword"], c["shield"]],
-            2,
-            10
+        ## locais originais ##
+        self.locations.append(Location( ## local da masmorra
+            "Masmorra Sombria",  
+            "Carrasco Imaculado", # inimigo da area
+            [c["sword"], c["shield"]], ## armas do inimigo
+            2, # vida do inimigo
+            10# recompensa
         ))
-        self.locations.append(Location(
+        self.locations.append(Location( ## local de castelo
             "Castelo Sombrio", 
-            "Rei Perdido",
-            [c["sword"], c["greatsword"]],
-            5,
-            40
+            "Rei Perdido", # inimigo da area
+            [c["sword"], c["greatsword"]], ## armas do inimigo
+            5, # vida do inimigo
+            40# recompensa
         ))
-        self.locations.append(Location(
+        self.locations.append(Location( ## local da floresta
             "Floresta Negra", 
-            "Urso Ancião",
-            [c["shield"], c["sword"]],
-            3,
-            20
+            "Urso Ancião", # inimigo da area
+            [c["shield"], c["sword"]], ## urso com escudo e espada maneiro.
+            3, # vida do inimigo
+            20 # recompensa
         ))
